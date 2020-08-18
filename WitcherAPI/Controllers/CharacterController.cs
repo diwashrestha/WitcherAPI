ba3d7de34e5022ede0eff5f24badb38a02d73adb
diwashrestha <diwash712@gmail.com>
2020-08-18 22:15:42 +0545

    Added Character Controller

---
+++ WitcherAPI/Controllers/CharacterController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WitcherAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CharacterController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllChracters()
+        {
+            return Ok("Character");
+        }
+    }
+}
---
+++ WitcherAPI/Models/Character.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WitcherAPI.Models
+{
+    public class Character
+    {
+        public int CharId { get; set; }
+        public string Name { get; set; }
+        public string Sex { get; set; }
+        public string Nationality { get; set; }
+        public string Hair { get; set; }
+        public string Race { get; set; }
+        public string Description { get; set; }
+    }
+}
