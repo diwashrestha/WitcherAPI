defa52772bb38034005bf67b0d74142ba9ebd99c
diwashrestha <diwash712@gmail.com>
2020-09-21 12:32:25 +0545

    added xml documentation comment

--- WitcherAPI/Startup.cs
+++ WitcherAPI/Startup.cs
@@ -22,25 +22,40 @@ namespace WitcherAPI
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        [Obsolete]
         public void ConfigureServices(IServiceCollection services)
         {
-            var connection = Configuration.GetConnectionString("WitcherDatabase");
-            services.AddDbContext<WitcherDbContext>(options => options.UseSqlServer(connection));
-            services.AddControllers();
+            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+            {
+                var connection = Configuration.GetConnectionString("WitcherDBProd");
+                services.AddDbContext<WitcherDbContext>(options => options.UseSqlServer(connection));
+                services.AddControllers();
+            }
+            else
+            {
+                var connection = Configuration.GetConnectionString("WitcherDatabase");
+                services.AddDbContext<WitcherDbContext>(options => options.UseSqlServer(connection));
+                services.AddControllers();
+            }
+
+
 
             // Register the Swaager generator
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo
                 {
-                    Title = "Witcher API",
-                    Description = "A Rest API  for the Witcher 3 Game. You can get data about the character, monsters, weapons, armors in the game.",
+                    Title = "Witcher 3 API",
+                    Description = "<img src=\"https://raw.githubusercontent.com/diwashrestha/WitcherAPI/master/witcherapi.png\" /> " +
+                    "<br><br> The Witcher 3 API catalogs the characters, weapons, monsters,armors found in the world of the  <a href=\"https://thewitcher.com/en/witcher3\">The Witcher 3: Wild Hunt</a> Game. " +
+                    "It is created as fun project to help users discover the world of witcher and consume data using the HTTP requests and interact with them.",
                     Contact = new OpenApiContact
                     {
                         Name = "Diwash Shrestha",
                         Url = new Uri("http://diwashrestha.com.np/"),
                     }
                 });
+                c.IncludeXmlComments(string.Format(@"bin\WitcherAPI.xml"));
             });
 
         }
